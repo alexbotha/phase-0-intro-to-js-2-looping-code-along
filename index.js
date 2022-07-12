8cade4f2b4213198d078d594daeb9337b7119232
alexbotha <alexbotha@hotmail.co.uk>
2022-07-12 16:13:09 +0100

    commit tree 34bf9cf5ee1062426377378b5e5d557827af32a4

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const cardNames = (['Guadalupe', 'Ollie', 'Aki'], "surprise");
+const copyOfCardNames = []
+
+function writeCards(cardNames) { 
+  for (let i = 0; i < cardNames.length; i++) {
+  copyOfCardNames.push(`Thank you, ${cardNames[i]}, for the wonderful surprise gift!`);
+  }
+  
+  return (copyOfCardNames);
+}
+writeCards(copyOfCardNames);
+
+/// ----------------------------CountDown()
+
+
+
+function countDown(count) {
+  while (count>= 0) {
+    console.log(count);
+    count--;
+  }
+}
